9d35e0515996385ce43f6e327d5fd5779050a0b7
mihneacojocaru <mihnea.co@gmail.com>
2021-09-15 19:59:05 +0200

    added sections

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,6 +1,35 @@
-let btnClose = document.addEventListener("click", (e) => {
+let btnClose = document.addEventListener("click", e => {
   let obj = e.target;
   if (obj.id == "alertClose") {
     obj.parentNode.classList.add("moveRight");
+    setTimeout(() => {
+      obj.parentNode.style.display = "none";
+    }, 500);
+  }
+});
+
+let trafficButtons = document.addEventListener("click", element => {
+  let obj = element.target;
+  let allSpans = document.querySelectorAll(".buttons > span");
+  if (obj.textContent == "Hourly") {
+    allSpans.forEach(element => {
+      element.classList.remove("selectedBtn");
+    });
+    obj.classList.add("selectedBtn");
+  } else if (obj.textContent == "Daily") {
+    allSpans.forEach(element => {
+      element.classList.remove("selectedBtn");
+    });
+    obj.classList.add("selectedBtn");
+  } else if (obj.textContent == "Weekly") {
+    allSpans.forEach(element => {
+      element.classList.remove("selectedBtn");
+    });
+    obj.classList.add("selectedBtn");
+  } else if (obj.textContent == "Monthly") {
+    allSpans.forEach(element => {
+      element.classList.remove("selectedBtn");
+    });
+    obj.classList.add("selectedBtn");
   }
 });
